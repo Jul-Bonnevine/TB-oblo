fddeb63e70fa8acec30439139fedda1279e50afa
Jul_Bonnevine <132055145+Jul-Bonnevine@users.noreply.github.com>
2025-06-27 15:59:23 +0200

    Update_ADC.cpp

--- project_oblo/src/ADC.cpp
+++ project_oblo/src/ADC.cpp
@@ -57,37 +57,39 @@ bool ADC::readRaw(uint16_t& value)
     /*
         Pour l'ADC. Il faut mettre cette ligne. Il n'y a pas de décalage 
     */
-    //value = ((rx[0] & 0x0F) << 8) | rx[1];  // 4 bits de padding + 12 bits MSB-first
+    value = ((rx[0] & 0x0F) << 8) | rx[1];  // 4 bits de padding + 12 bits MSB-first
 
     /*
         Pour la génération avec waveform utiliser cette ligne. Un décalage est obligatoire
         à cause d'un mauvais timing
     */
-    value = (((rx[0] << 8) | rx[1]) << 1) & 0x0FFF;
+    //value = (((rx[0] << 8) | rx[1]) << 1) & 0x0FFF;
 
     return true;
 }
 
 
-float ADC::readTemperature()
+float ADC::readTemperature(uint16_t adc_val)
 {
-    uint16_t adc_val = 0;
-    if (!readRaw(adc_val)) {
-        std::cerr << "Erreur lecture ADC\n";
-        return 0.0f;
-    }
-
-    // Étape 1 : conversion valeur ADC → tension
     const float adc_resolution = 4096.0f;
     float Vout = (adc_val * Vcc) / adc_resolution;
-
-    // Étape 2 : calcul résistance sonde
     float Vref = Vcc;
+
     float Rsonde = ((-1.0f * (Vout * (R_fils + R_fixe) - Vref * R_fils)) /
                    (Vout - Vref));
+    float Rntc = Rsonde - R_fils;
 
-    // Étape 3 : calcul température avec loi de β
-    float tempK = (beta * T25) / (log((Rsonde - R_fils) / R25) * T25 + beta);
+    std::cout << "ADC brut: " << adc_val << "\n";
+    std::cout << "Vout: " << Vout << " V\n";
+    std::cout << "Rsonde: " << Rsonde << " ohms\n";
+    std::cout << "Rsonde - R_fils: " << Rntc << " ohms\n";
+
+    if (Rntc <= 0.0f) {
+        std::cerr << "[ADC] Résistance NTC invalide : " << Rntc << " ohms\n";
+        return NAN;
+    }
+
+    float tempK = (beta * T25) / (log(Rntc / R25) * T25 + beta);
     float tempC = tempK - 273.15f;
 
     return tempC;
